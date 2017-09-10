dfd75c05b433d0335d96fa7b86d8199a1403c875
smoisset <smoisset@worldremit.com>
2017-09-10 12:01:52 +0100

    Design Blog Pages

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -1,96 +1,109 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+    <title>Laravel Blog</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="/">Laravel Blog</a>
+            </div>
 
-            .full-height {
-                height: 100vh;
-            }
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="/">Home <span class="sr-only">(current)</span></a></li>
+                <li><a href="/about">About</a></li>
+                <li><a href="/contact">Contact</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">My Account <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="#">Action</a></li>
+                    <li><a href="#">Another action</a></li>
+                    <li><a href="#">Something else here</a></li>
+                    <li role="separator" class="divider"></li>
+                    <li><a href="#">Separated link</a></li>
+                </ul>
+                </li>
+            </ul>
+            </div>
+        </div>
+    </nav>
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Welcome Everyone!
-                </div>
-                <p>This is the beginning of your Laravel Journey</p>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="jumbotron">
+                    <h1>Welcome to My Blog!</h1>
+                    <p class="lead">Thank you for visiting. This is my test website built with Laravel. Please read my latest post!</p>
+                    <p><a class="btn btn-primary btn-lg" href="#" role="button">Popular Post</a></p>
                 </div>
             </div>
         </div>
-    </body>
+
+        <div class="row">
+            <div class="col-md-8">
+                <div class="post">
+                    <h3>Post Title</h3>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat, recusandae! Aut placeat in omnis quos est sapiente molestiae facere vel mollitia dolore maxime nesciunt similique quod veritatis sit, distinctio itaque?</p>
+                    <a href="#" class="btn btn-primary">Read More</a>
+                </div>
+
+                <hr>
+
+                <div class="post">
+                    <h3>Post Title</h3>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat, recusandae! Aut placeat in omnis quos est sapiente molestiae facere vel mollitia dolore maxime nesciunt similique quod veritatis sit, distinctio itaque?</p>
+                    <a href="#" class="btn btn-primary">Read More</a>
+                </div>
+
+                <hr>
+
+                <div class="post">
+                    <h3>Post Title</h3>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat, recusandae! Aut placeat in omnis quos est sapiente molestiae facere vel mollitia dolore maxime nesciunt similique quod veritatis sit, distinctio itaque?</p>
+                    <a href="#" class="btn btn-primary">Read More</a>
+                </div>
+
+                <hr>
+
+                <div class="post">
+                    <h3>Post Title</h3>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat, recusandae! Aut placeat in omnis quos est sapiente molestiae facere vel mollitia dolore maxime nesciunt similique quod veritatis sit, distinctio itaque?</p>
+                    <a href="#" class="btn btn-primary">Read More</a>
+                </div>
+
+                <hr>
+            </div>
+
+            <div class="col-md-3 col-md-offset-1">
+                <h2>Sidebar</h2>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+  </body>
 </html>
