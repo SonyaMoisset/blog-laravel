bf5d0c53d4ece9bf17ea15ce53679680de6687ed
smoisset <smoisset@worldremit.com>
2017-09-12 08:44:20 +0100

    Add store action

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostController extends Controller
 {
@@ -34,7 +35,22 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // Validate the data
+        $this->validate($request, array(
+            'title' => 'required|max:255',
+            'body' => 'required'
+        ));
+
+        // Store in the db
+        $post = new Post;
+
+        $post->title = $request->title;
+        $post->body = $request->body;
+
+        $post->save();
+
+        // Redirect to another page
+        return redirect()->route('posts.show', $post->id);
     }
 
     /**
